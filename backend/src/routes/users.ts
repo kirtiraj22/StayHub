bda7fba33dfff07ce9ea30dc5a7d6b8d5646a50e
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-07 22:24:37 +0530

    backend/feat: added validation for registeration endpoint using express-validator

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,46 +1,68 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
 const router = express.Router();
 
 // /api/users/register
-router.post("/register", async (req: Request, res: Response) => {
-	try {
-		let user = await User.findOne({
-			email: req.body.email,
-		});
+router.post(
+	"/register",
+	[
+		check("firstName", "First Name is required").isString(),
+		check("lastName", "Last Name is required").isString(),
+		check("email", "Email is required").isEmail(),
+		check(
+			"password",
+			"Password with 6 or more characters required"
+		).isLength({ min: 6 }),
+	],
+	async (req: Request, res: Response) => {
+		// get errors from express validators(if any)
+		const errors = validationResult(req);
+		// if errors is not empty, return status 400
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ message: errors.array() });
+		}
+		try {
+			let user = await User.findOne({
+				email: req.body.email,
+			});
 
-		if (user) {
-			return res.status(400).json({
-				message: "User already exists",
+			if (user) {
+				return res.status(400).json({
+					message: "User already exists",
+				});
+			}
+
+			// adding all the fields(firstName,email,etc) directly from the request body
+			user = new User(req.body);
+			await user.save();
+
+			const token = jwt.sign(
+				{ userId: user.id },
+				process.env.JWT_SECRET_KEY as string,
+				{
+					expiresIn: "1d",
+				}
+			);
+
+			res.cookie("auth_token", token, {
+				httpOnly: true,
+				// use secure only when the code is in production
+				secure: process.env.NODE_ENV === "production",
+				// 864000000 in milliseconds(i.e 1 day)
+				maxAge: 86400000,
+			});
+
+			return res.sendStatus(200);
+		} catch (error) {
+			console.log(error);
+			res.status(500).send({
+				message: "Something went wrong!",
 			});
 		}
-
-		user = new User(req.body);
-		await user.save();
-
-		const token = jwt.sign(
-			{ userId: user.id },
-			process.env.JWT_SECRET_KEY as string,
-			{
-				expiresIn: "1d",
-			}
-		);
-
-		res.cookie("auth_token", token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			// 864000000 in milliseconds(i.e 1 day)
-			maxAge: 86400000,
-		});
-
-		return res.sendStatus(200);
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({
-			message: "Something went wrong!",
-		});
 	}
-});
+);
 
 export default router;
