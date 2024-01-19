629eb0f1177ecdfdf720b0f0eb5eca2f89782ad7
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-19 11:50:24 +0530

    frontend: added first portion of hotel Form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,10 @@ import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
-
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 function App() {
+	const { isLoggedIn } = useAppContext();
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -39,6 +41,18 @@ function App() {
 						</Layout>
 					}
 				/>
+				{isLoggedIn && (
+					<>
+						<Route
+							path="/add-hotel"
+							element={
+								<Layout>
+									<AddHotel />
+								</Layout>
+							}
+						/>
+					</>
+				)}
 				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</BrowserRouter>
