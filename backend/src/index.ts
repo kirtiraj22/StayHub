1209012d00041f1f0998326ae1affdb774f73cb5
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-22 11:50:56 +0530

    backend/feat: implement catch-all route for serving main application page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,6 +36,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+	res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(3000, () => {
 	console.log("App running on localhost: 3000");
 });
