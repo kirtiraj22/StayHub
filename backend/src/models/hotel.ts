fd35cb247057759bc5d21c67343057c6faae45a2
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-23 12:30:26 +0530

    frontend: added my-hotels page

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -14,7 +14,7 @@ export type HotelType = {
 	starRating: number;
 	imageUrls: string[];
 	lastUpdated: Date;
-	newProperty: string;
+	// newProperty: string;
 };
 
 const hotelSchema = new mongoose.Schema<HotelType>({
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 function App() {
 	const { isLoggedIn } = useAppContext();
 	return (
@@ -51,8 +52,17 @@ function App() {
 								</Layout>
 							}
 						/>
+						<Route
+							path="/my-hotels"
+							element={
+								<Layout>
+									<MyHotels />
+								</Layout>
+							}
+						/>
 					</>
 				)}
+
 				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</BrowserRouter>
