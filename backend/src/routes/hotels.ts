61fe48fad7745448a2754846e29f19dadb29f02c
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-29 13:27:59 +0530

    backend: logic for sort and filter function(hotels)

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, query } from "express";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
 
@@ -6,13 +6,32 @@ const router = express.Router();
 
 router.get("/search", async (req: Request, res: Response) => {
 	try {
+		const query = constructSearchQuery(req.query);
+
+		let sortOptions = {};
+		switch (req.query.Option) {
+			case "starRating":
+				// sorts high to low
+				sortOptions = { starRating: -1 };
+				break;
+			case "pricePerNightAsc":
+				sortOptions = { pricePerNight: 1 };
+				break;
+			case "pricePerNightDesc":
+				sortOptions = { pricePerNight: -1 };
+				break;
+		}
+
 		const pageSize = 5;
 		const pageNumber = parseInt(
 			req.query.page ? req.query.page.toString() : "1"
 		);
 		const skip = (pageNumber - 1) * pageSize;
 
-		const hotels = await Hotel.find().skip(skip).limit(pageSize);
+		const hotels = await Hotel.find(query)
+			.sort(sortOptions)
+			.skip(skip)
+			.limit(pageSize);
 
 		const total = await Hotel.countDocuments();
 
@@ -35,5 +54,58 @@ router.get("/search", async (req: Request, res: Response) => {
 		});
 	}
 });
+
+const constructSearchQuery = (queryParams: any) => {
+	let constructedQuery: any = {};
+	if (queryParams.destination) {
+		// This condition searches for documents where either the city or country fields match the provided destination(using case-insensitive regex)
+		constructedQuery.$or = [
+			{ city: new RegExp(queryParams.destination, "i") },
+			{ country: new RegExp(queryParams.destination, "i") },
+		];
+	}
+	if (queryParams.adultCount) {
+		constructedQuery.adultCount = {
+			// greater than or equal to specified count
+			$gte: parseInt(queryParams.adultCount),
+		};
+	}
+	if (queryParams.childCount) {
+		constructedQuery.childCount = {
+			$gte: parseInt(queryParams.childCount),
+		};
+	}
+
+	if (queryParams.facilities) {
+		// checks for documents where the facilities field matches all the specified values provided in queryParams.facilities
+		constructedQuery.facilities = {
+			$all: Array.isArray(queryParams.facilities)
+				? queryParams.facilities
+				: [queryParams.facilities],
+		};
+	}
+
+	if (queryParams.types) {
+		constructedQuery.type = {
+			$in: Array.isArray(queryParams.types)
+				? queryParams.types
+				: [queryParams.types],
+		};
+	}
+
+	if (queryParams.stars) {
+		const starRating = parseInt(queryParams.stars.toString());
+		// eq : equal to specified number(starRating)
+		constructedQuery.starRating = { $eq: starRating };
+	}
+
+	if (queryParams.maxPrice) {
+		constructedQuery.maxPrice = {
+			$lte: parseInt(queryParams.maxPrice).toString(),
+		};
+	}
+
+	return constructedQuery;
+};
 
 export default router;
