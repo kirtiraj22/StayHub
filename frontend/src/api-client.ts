2b46e409d6fa69e9baca820a30645a3b7195ea94
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-30 11:46:21 +0530

    frontend/feat: logic to filter hotels by stars

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -126,6 +126,11 @@ export type SearchParams = {
 	adultCount?: string;
 	childCount?: string;
 	page?: string;
+	facilities?: string[];
+	types?: string[];
+	stars?: string[];
+	maxPrice?: string;
+	sortOptions?: string;
 };
 
 export const searchHotels = async (
@@ -139,6 +144,16 @@ export const searchHotels = async (
 	queryParams.append("adultCount", searchParams.adultCount || "");
 	queryParams.append("childCount", searchParams.childCount || "");
 	queryParams.append("page", searchParams.page || "");
+
+	queryParams.append("maxPrice", searchParams.maxPrice || "");
+	queryParams.append("sortOptions", searchParams.sortOptions || "");
+
+	searchParams.facilities?.forEach((facility) =>
+		queryParams.append("facilities", facility)
+	);
+
+	searchParams.types?.forEach((type) => queryParams.append("types", type));
+	searchParams.stars?.forEach((star) => queryParams.append("stars", star));
 
 	// send get request with the all the parameters
 	const response = await fetch(
