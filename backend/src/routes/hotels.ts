df833d63f0014a3b51b83064f5126af4c241b9c9
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-01-31 12:45:07 +0530

    frontend: fetch hotel by id && display hotel details

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -5,31 +5,6 @@ import { param, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.get(
-	"/:id",
-	[param("id").notEmpty().withMessage("Hotel ID is required")],
-	async (req: Request, res: Response) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({
-				errors: errors.array(),
-			});
-		}
-
-		const id = req.params.id.toString();
-
-		try {
-			const hotel = await Hotel.findById(id);
-			res.json(hotel);
-		} catch (err) {
-			console.log(err);
-			res.status(500).json({
-				message: "Error fetching hotel!",
-			});
-		}
-	}
-);
-
 router.get("/search", async (req: Request, res: Response) => {
 	try {
 		const query = constructSearchQuery(req.query);
@@ -80,6 +55,31 @@ router.get("/search", async (req: Request, res: Response) => {
 		});
 	}
 });
+
+router.get(
+	"/:id",
+	[param("id").notEmpty().withMessage("Hotel ID is required")],
+	async (req: Request, res: Response) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array(),
+			});
+		}
+
+		const id = req.params.id.toString();
+
+		try {
+			const hotel = await Hotel.findById(id);
+			res.json(hotel);
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({
+				message: "Error fetching hotel!",
+			});
+		}
+	}
+);
 
 const constructSearchQuery = (queryParams: any) => {
 	let constructedQuery: any = {};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
+import Detail from "./pages/Detail";
 function App() {
 	const { isLoggedIn } = useAppContext();
 	return (
@@ -25,6 +26,14 @@ function App() {
 					element={
 						<Layout>
 							<Search />
+						</Layout>
+					}
+				/>
+				<Route
+					path="/detail/:hotelId"
+					element={
+						<Layout>
+							<Detail />
 						</Layout>
 					}
 				/>
