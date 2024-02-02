6f7adce89be21db11e255b2c39d76eea9e28eacb
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-02-02 12:25:59 +0530

    backend/stripe: booking details schema && save bookings endpoint

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, query } from "express";
 import Hotel from "../models/hotel";
-import { HotelSearchResponse } from "../shared/types";
+import { BookingType, HotelSearchResponse } from "../shared/types";
 import { param, validationResult } from "express-validator";
 import Stripe from "stripe";
 import verifyToken from "../middleware/auth";
@@ -127,6 +127,70 @@ router.post(
 		};
 
 		res.send(response);
+	}
+);
+
+router.post(
+	"/:hotelId/bookings",
+	verifyToken,
+	async (req: Request, res: Response) => {
+		try {
+			const paymentIntentId = req.body.paymentIntentId;
+			const paymentIntent = await stripe.paymentIntents.retrieve(
+				paymentIntentId as string
+			);
+
+			if (!paymentIntent) {
+				return res.status(400).json({
+					message: "payment intent not found!",
+				});
+			}
+
+			if (
+				paymentIntent.metadata.hotelId !== req.params.hotelId ||
+				paymentIntent.metadata.userId !== req.userId
+			) {
+				return res.status(400).json({
+					message: "payment intent mismatch",
+				});
+			}
+
+			if (paymentIntent.status !== "succeeded") {
+				return res.status(400).json({
+					message: `payment intent not succeeded. Status : ${paymentIntent.status}`,
+				});
+			}
+
+			const newBooking: BookingType = {
+				...req.body,
+				userId: req.userId,
+			};
+
+			const hotel = await Hotel.findOneAndUpdate(
+				{
+					_id: req.params.hotelId,
+				},
+				{
+					$push: {
+						booking: newBooking,
+					},
+				}
+			);
+
+			if (!hotel) {
+				return res.status(400).json({
+					message: "hotel not found!",
+				});
+			}
+
+			await hotel.save();
+			res.status(200).send();
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({
+				message: "Something went wrong",
+			});
+		}
 	}
 );
 
