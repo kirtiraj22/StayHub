12b7d630db0c97eb1a74a35da9460108eee27df7
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-02-13 13:27:33 +0530

    frontend/backend: get all hotels && home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 function App() {
 	const { isLoggedIn } = useAppContext();
 	return (
@@ -19,7 +20,7 @@ function App() {
 					path="/"
 					element={
 						<Layout>
-							<p>Home page</p>
+							<Home />
 						</Layout>
 					}
 				/>
